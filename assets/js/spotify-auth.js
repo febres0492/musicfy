6111cb843556b98284b9966379790875fae082ff
Carlos Febres <febres0492@gmail.com>
2024-04-10 19:20:39 -0400

    adding spotify-auth.js

---
+++ assets/js/spotify-auth.js
@@ -0,0 +1,13 @@
+
+const spotifyAuthUrl = 'https://musicfy-auth.netlify.app/.netlify/functions/spotify-auth' 
+async function getSpotifyToken () {
+    const response = await axios.get(spotifyAuthUrl)
+    const token = response.data.access_token
+    localStorage.setItem('spotify_access_token', token)
+}
+getSpotifyToken()
+
+function test(){
+    console.log('test')
+}
+
