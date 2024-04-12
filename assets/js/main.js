774e786d2032a675dcd425d02a5cbb6dc0966b48
Carlos Febres <febres0492@gmail.com>
2024-04-12 19:11:07 -0400

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,61 @@
+const userFormEl = document.querySelector('#searchBtn');
+const languageButtonsEl = document.querySelector('#language-buttons');
+const nameInputEl = document.querySelector('#searchBar');
+const repoContainerEl = document.querySelector('#video-results');
+const repoSearchTerm = document.querySelector('#repo-search-term');
 
+// New code
+//using async, await to wait until the function is complete
+const getReposByTopic = async (topic) => {
 
-function getSpotifyData () {
-    const token = localStorage.getItem('spotify_access_token')
-    const query = 'Nirvana'; 
-    const searchType = 'artist'; // Could be 'album', 'track', 'playlist', etc.
+    const link = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyD7neHHqfKylKN206rx0tnSRa5uq1nvmoY&part=snippet&maxResults=30&q=${topic}&type=video&order=title&videoEmbeddable=true&location=40.730610, -73.935242&location_radius=100km`
+    const response = await axios.get(link)
+  
+      let repos = response.data.items
 
-    fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=${searchType}`, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data); // Process search results here
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    displayRepos(repos)
+    showVideoOnClick()
 }
-// getSpotifyData()
+
+
+
+const displayRepos = (repos) => {
+    repoContainerEl.innerHTML = ``;
+    repos.forEach((repo) => {
+    let url = `https://www.youtube.com/watch?v=${repo.id.videoId}`;
+    url = url.replace("watch?v=", "embed/");
+    
+
+    repoContainerEl.innerHTML += 
+    
+    `
+        <div class="yt-results yt-link border bg-secondary mb-3 rounded p-3" data-link="${url}">
+            <span>${repo.snippet.title}</span>  
+            <img src="${repo.snippet.thumbnails.medium.url}"
+        </div>
+    `;
+  })
+}
+
+
+
+userFormEl.addEventListener('click', (event) => {
+    event.preventDefault();
+    const user = nameInputEl.value;
+    console.log(`user form is clicked.`, user)
+    if (user) {getReposByTopic(user)}
+})
+
+function showVideoOnClick(){
+    
+  [...$('.yt-link')].forEach(el => {
+    el.addEventListener('click', (event) => {
+        event.preventDefault();
+        console.log(`link was clicked`)
+        const el = event.target.closest('.yt-link')
+        const link = el.getAttribute('data-link')
+        $('#iframe')[0].setAttribute('src', link)
+      })
+  } )  
+}
+
